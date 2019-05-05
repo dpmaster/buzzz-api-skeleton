8f3234b195749e15459ae1578ba38a957135a54e
Ruslan Skolotiy <skolotiy@media5.com>
2019-05-05 20:33:52 +0300

    test get/post actions

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,4 +8,12 @@ class IndexController extends Controller {
     public function indexAction($request_data, $url_params) {
         return ['status' => "ok", 'test_di' => $this->test_di, 'request_data' => $request_data, 'url_params' => $url_params];
     }
+
+    public function testGetAction() {
+        return ['it is GET request'];
+    }
+
+    public function testPostAction() {
+        return ['it is POST request'];
+    }
 }
